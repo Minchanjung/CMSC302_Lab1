cb720322913ded5846743a6d5b7b3b5e1b599d1c
MinchanJung <minchanjung1@gmail.com>
2025-02-24 16:24:58 +0000

    task 2 and 3

--- src/MovieDriver.java
+++ src/MovieDriver.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class MovieDriver {
 
 	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+		Movie userMovie = new Movie();
+		String playAgain = "";
 		
-
+		do {
+			
+			System.out.print("Enter the title of a movie: ");
+			String movie = keyboard.nextLine();
+			userMovie.setTitle(movie);
+			
+			System.out.print("Enter the movie's rating: ");
+			String movieRating = keyboard.next();
+			userMovie.setRating(movieRating);
+	
+			System.out.print("Enter the number of tickets sold at the theater: ");
+			int ticketsSold = keyboard.nextInt();
+			userMovie.setSoldTickets(ticketsSold);
+			
+			System.out.println(userMovie.toString());
+			
+			keyboard.nextLine();
+			System.out.print("\nWould you like to enter another (y or n): " );
+			playAgain = keyboard.nextLine();
+		
+		} while (playAgain.equals("y"));
+		keyboard.close();
 	}
 
 }
